2674094d767e4c01d37e7491224da16ec697603b
Siroko <sirokosan@gmail.com>
2017-04-13 11:07:16 +0200

    add more options to framebufferobject

--- src/js/gl/core/Constants.js
+++ src/js/gl/core/Constants.js
@@ -2,10 +2,21 @@
  * Created by sirok on 05/02/2017.
  */
 
-export const GL_TRIANGLES_PRIMITIVE     = 'triangles';
-export const GL_LINES_PRIMITIVE         = 'lines';
-export const GL_LINE_STRIP_PRIMITIVE    = 'line_strip';
-export const GL_POINTS_PRIMITIVE        = 'points';
+export const GL_TRIANGLES_PRIMITIVE                 = 'triangles';
+export const GL_LINES_PRIMITIVE                     = 'lines';
+export const GL_LINE_STRIP_PRIMITIVE                = 'line_strip';
+export const GL_POINTS_PRIMITIVE                    = 'points';
 
-export const GL_FLOAT_TYPE              = 'float';
-export const GL_HALF_FLOAT_TYPE         = 'half_float';
+export const GL_FLOAT_TYPE                          = 'float';
+export const GL_HALF_FLOAT_TYPE                     = 'half_float';
+
+export const GL_RGBA_FORMAT                         = 'rgba_format';
+
+export const GL_NEAREST_FILTER                      = 'nearest_filter';
+export const GL_LINEAR_FILTER                       = 'linear_filter';
+export const GL_NEAREST_MIPMAP_NEAREST_FILTER       = 'nearest_mipmap_nearest_filter';
+export const GL_NEAREST_MIPMAP_LINEAR_FILTER        = 'nearest_mipmap_linear_filter';
+export const GL_LINEAR_MIPMAP_NEAREST_FILTER        = 'linear_mipmap_nearest_filter';
+export const GL_LINEAR_MIPMAP_LINEAR_FILTER         = 'linear_mipmap_linear_filter';
+
+export const GL_CLAMP_TO_EDGE                       = 'clamp_to_edge';
